347801c3ce8d231d40a9954643af52dc7b86c172
Denis Khodakov <denis5517@yandex.ru>
2022-08-28 18:42:42 +0700

    new

---
+++ homework 6.6/homework 6.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введем 1 - вывести данные на экран");
+            Console.WriteLine("Введем 2 - заполнить данные и добавить новую запись в конец файла");
+        }
+    }
+}
